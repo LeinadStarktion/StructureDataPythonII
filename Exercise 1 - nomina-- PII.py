d02068ae47ae6b5937ac7772033959de1fffb1d5
LeinadStarktion <wolf1313@outlook.com>
2022-07-29 19:24:58 -0500

    Se término el calculo de la nómina

--- Exercise 1 - nomina-- PII.py
+++ Exercise 1 - nomina-- PII.py
@@ -1,3 +1,6 @@
+from turtle import position
+
+
 Legal = [1000000]
 
 Health = [0.04]
@@ -5,19 +8,20 @@ Pension = [0.04]
 
 CC = (int(input("Write your ID number")))
 
-while name.isalpha() != True:
-    print("What is your name? ")
-    name = input()
 
-    print("What is your lastname?")
+print("What is your name? ")
+name = input()
+ 
+print("What is your lastname?")
 last = input()
-while last.isalpha() != True:
 
-    print("what is your salary? ")
+print("What is your position?")
+position = input()
+
+print("what is your salary? ")
 salary = int(input())
-while salary.isalpha() != True:
 
-    print("How many days did you work?")
+print("How many days did you work?")
 WD = int(input())
 
 if salary == 1000000:
@@ -26,14 +30,21 @@ if salary == 1000000:
 else:
     print("Your do not have transportation benefit")
 
+print ("")
+print ("----------------------------------------------------")
+print ("|                                                  |")
+print (f"Name : {name}")
+print (f"Latname : {last}")
+print (f"Id number : {CC}")
+print (f"Position :{position}")
 SXD = (salary*WD)/30
-print(f"Your total salary for days worked:{SXD}")
+print(f"Your total salary for days worked :{SXD}")
 BXH = (SXD*0.04)
 print(f"Your benefit health is :{BXH}")
 BXP = (SXD*0.04)
 print(f"Your benefit pension is :{BXP}")
-print(f"Your total for benefits is: {BXH+BXP}")
-
-
-
-
+BXHP = BXH+BXP
+print (f"Your total benefits is : {BXHP}")
+print ("")
+print ("----------------------------------------------------")
+print ("|                                                  |")
